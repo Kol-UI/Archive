e61a9c0f7d62847ef64eb70c946686976c5d586e
Kol-UI <nicolas.aubry@live.fr>
2024-03-26 22:52:19 +0100

    Explore Enum C#

---
+++ C#/Enum/Explore Enumeration/EnumerationTest.cs
@@ -0,0 +1,8 @@
+using EnumerableClasses;
+
+Console.WriteLine("Enumerable Class Tutorial");
+MyEnumeration value = MyEnumeration.Value1;
+Console.WriteLine(value); // Output: 1: Value 1
+
+Console.WriteLine(value.Id); // Output: 1
+Console.WriteLine(value.Name); // Output: Value 1
