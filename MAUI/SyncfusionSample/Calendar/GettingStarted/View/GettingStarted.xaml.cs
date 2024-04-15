5165fcf3211a7be4faa4afbff709acaae1cb4db7
Kol-UI <nicolas.aubry@live.fr>
2024-04-15 21:15:42 +0200

    commit sync calendar workflow

---
+++ MAUI/SyncfusionSample/Calendar/GettingStarted/View/GettingStarted.xaml.cs
@@ -0,0 +1,18 @@
+using SampleBrowser.Maui.Base;
+
+namespace SampleBrowser.Maui.Calendar.SfCalendar
+{
+    /// <summary>
+    /// Interaction logic for GettingStarted.xaml
+    /// </summary>
+    public partial class GettingStarted : SampleView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GettingStarted" /> class.
+        /// </summary>
+        public GettingStarted()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI/SyncfusionSample/Calendar/Views/View/Year.xaml.cs
@@ -0,0 +1,52 @@
+using SampleBrowser.Maui.Base;
+using Syncfusion.Maui.Calendar;
+
+namespace SampleBrowser.Maui.Calendar.SfCalendar
+{
+    /// <summary>
+    /// Interaction logic for GettingStarted.xaml
+    /// </summary>
+    public partial class Year : SampleView
+    {
+        /// <summary>
+        /// Background color for border control. Border control will not shown on mobile platforms.
+        /// </summary>
+        public Color BGColor { get; set; }
+
+        /// <summary>
+        /// Stroke color for border control. Border control will not shown on mobile platforms.
+        /// </summary>
+        public Color StrokeColor { get; set; }
+
+        /// <summary>
+        /// Check the application theme is light or dark.
+        /// </summary>
+        private bool isLightTheme = Application.Current?.RequestedTheme == AppTheme.Light;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Year" /> class.
+        /// </summary>
+        public Year()
+        {
+            InitializeComponent();
+#if MACCATALYST || (!ANDROID && !IOS)
+            this.Background = isLightTheme ? Brush.White : (Brush)Color.FromRgba("#1C1B1F");
+            this.Margin = new Thickness(-4, -4, -6, -6);
+            this.BGColor = isLightTheme ? Color.FromArgb("#F7F2FB") : Color.FromArgb("#25232A");
+            this.StrokeColor = Colors.Transparent;
+#else
+            this.BGColor = Colors.Transparent;
+            this.StrokeColor = Colors.Transparent;
+#endif
+
+            this.BindingContext = this;
+#if IOS || MACCATALYST
+            this.border.IsVisible = true;
+            this.year1.SelectedDate = DateTime.Now;
+#else
+            this.frame.IsVisible = true;
+            this.year.SelectedDate = DateTime.Now;
+#endif
+        }
+    }
+}
