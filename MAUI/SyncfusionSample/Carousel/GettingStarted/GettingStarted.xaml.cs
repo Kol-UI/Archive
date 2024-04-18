215429900d88b523321904f015f59f88956fe321
Kol-UI <nicolas.aubry@live.fr>
2024-04-18 19:52:31 +0200

    carousel maui syncfusion

---
+++ MAUI/SyncfusionSample/Carousel/GettingStarted/GettingStarted.xaml.cs
@@ -0,0 +1,17 @@
+using SampleBrowser.Maui.Base;
+
+namespace SampleBrowser.Maui.Carousel.Carousel
+{
+    public partial class GettingStarted : SampleView
+    {
+        public GettingStarted()
+        {
+            InitializeComponent();
+#if ANDROID || IOS
+            this.Content = new GettingStartedMobile();
+#elif WINDOWS || MACCATALYST
+            this.Content = new GettingStartedDesktop();
+#endif
+        }
+    }
+}
