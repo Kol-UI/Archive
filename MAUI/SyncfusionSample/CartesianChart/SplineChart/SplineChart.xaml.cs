3167e50bde94aaefbe83a96fb42bd3cc82aea5f6
Kol-UI <nicolas.aubry@live.fr>
2024-05-22 22:40:29 +0200

    syncfusion charts maui

---
+++ MAUI/SyncfusionSample/CartesianChart/SplineChart/SplineChart.xaml.cs
@@ -0,0 +1,34 @@
+
+using SampleBrowser.Maui.Base;
+using Syncfusion.Maui.Charts;
+
+namespace SampleBrowser.Maui.CartesianChart.SfCartesianChart
+{
+    public partial class SplineChart : SampleView
+    {
+        public SplineChart()
+        {
+            InitializeComponent();
+        }
+
+        public override void OnAppearing()
+        {
+            base.OnAppearing();
+            hyperLinkLayout.IsVisible = !IsCardView;
+#if IOS
+            if (IsCardView)
+            {
+                Chart.WidthRequest = 350;
+                Chart.HeightRequest = 400;
+                Chart.VerticalOptions = LayoutOptions.Start;
+            }
+#endif
+        }
+
+        public override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Chart.Handler?.DisconnectHandler();
+        }
+    }
+}
