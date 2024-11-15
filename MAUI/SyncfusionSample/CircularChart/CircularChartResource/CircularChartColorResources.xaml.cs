448190f779e88a15b5c08177c118f631c959c524
Kol-UI <nicolas.aubry@live.fr>
2024-11-15 22:17:43 +0100

    add circular chart

---
+++ MAUI/SyncfusionSample/CircularChart/CircularChartResource/CircularChartColorResources.xaml.cs
@@ -0,0 +1,10 @@
+
+namespace SampleBrowser.Maui.CircularChart.SfCircularChart;
+
+public partial class CircularChartColorResources : ResourceDictionary
+{
+	public CircularChartColorResources()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MAUI/SyncfusionSample/CircularChart/Interaction/Tooltip.xaml.cs
@@ -0,0 +1,20 @@
+
+using Microsoft.Maui.Controls;
+using SampleBrowser.Maui.Base;
+
+namespace SampleBrowser.Maui.CircularChart.SfCircularChart
+{ 
+    public partial class Tooltip : SampleView
+    {
+        public Tooltip()
+        {
+            InitializeComponent();
+        }
+
+        public override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            Chart.Handler?.DisconnectHandler();
+        }
+    }
+}
